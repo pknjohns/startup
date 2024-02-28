57a4c49450fe94155f18b3635917d5a08c599233
Parker Johns <pknjohns@byu.edu>
2024-02-28 14:31:46 -0700

    Copied code from simon scores.js to startup history.js to update history table with users dating history

---
+++ history.js
@@ -0,0 +1,35 @@
+function loadHist() {
+    let hist = [];
+    const histText = localStorage.getItem('hist');
+    if (histText) {
+      hist = JSON.parse(histText);
+    }
+  
+    const tableBodyEl = document.querySelector('#history');
+  
+    if (hist.length) {
+      for (const [i, hist] of hist.entries()) {
+        const positionTdEl = document.createElement('td');
+        const nameTdEl = document.createElement('td');
+        const activityTdEl = document.createElement('td');
+        const dateTdEl = document.createElement('td');
+  
+        positionTdEl.textContent = i + 1;
+        nameTdEl.textContent = score.name;
+        activityTdEl.textContent = score.score;
+        dateTdEl.textContent = score.date;
+  
+        const rowEl = document.createElement('tr');
+        rowEl.appendChild(positionTdEl);
+        rowEl.appendChild(nameTdEl);
+        rowEl.appendChild(activityTdEl);
+        rowEl.appendChild(dateTdEl);
+  
+        tableBodyEl.appendChild(rowEl);
+      }
+    } else {
+      tableBodyEl.innerHTML = '<tr><td colSpan=4>Be the first to get inspired for your next date!</td></tr>';
+    }
+  }
+  
+  loadHist();
