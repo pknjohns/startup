66bec738d32109cca252a464e46b516d551d3e9e
Parker Johns <pknjohns@byu.edu>
2024-02-28 21:47:27 -0700

    added getCommunity and getNoPrep functions to generate the date ideas for those corresponding categories

--- generator.js
+++ generator.js
@@ -58,3 +58,87 @@ function getOutdoor() {
     selectEl.add(idea5);
     selectEl.add(idea6);
 }
+
+function getNoPrep() {
+
+    // Get ideas dropdown
+    const selectEl = document.getElementById('ideas');
+
+    const def = document.createElement('option');
+    def.textContent = 'Which idea will you pick...?';
+
+    const idea1 = document.createElement('option');
+    idea1.text = 'Watch a movie';
+
+    const idea2 = document.createElement('option');
+    idea2.text = 'Go on a walk';
+
+    const idea3 = document.createElement('option');
+    idea3.text = 'Look at pictures of each other when you were young';
+
+    const idea4 = document.createElement('option');
+    idea4.text = 'Find songs that describe the other person or your relationship';
+
+    const idea5 = document.createElement('option');
+    idea5.text = 'Walk around Barnes & Noble or a grocery store and point out your favorite things to each other';
+
+    const idea6 = document.createElement('option');
+    idea6.text = 'Write a story using only emojis';
+
+    //this is the div container where the select elements are held
+    const container = document.getElementById('dates');
+
+    //Remove current ideas in ideas dropdown
+    clear(selectEl)
+
+    //Add new ideas to dropdown
+    selectEl.add(def);
+    selectEl.add(idea1);
+    selectEl.add(idea2);
+    selectEl.add(idea3);
+    selectEl.add(idea4);
+    selectEl.add(idea5);
+    selectEl.add(idea6);
+}
+
+function getCommunity() {
+
+    // Get ideas dropdown
+    const selectEl = document.getElementById('ideas');
+
+    const def = document.createElement('option');
+    def.textContent = 'Which idea will you pick...?';
+
+    const idea1 = document.createElement('option');
+    idea1.text = 'Go to a BYU sports event';
+
+    const idea2 = document.createElement('option');
+    idea2.text = 'Go to a BYU arts performance';
+
+    const idea3 = document.createElement('option');
+    idea3.text = 'Visit the Museum of Art';
+
+    const idea4 = document.createElement('option');
+    idea4.text = 'Go to a BYU devotional/ forum';
+
+    const idea5 = document.createElement('option');
+    idea5.text = "Visit a city you've never been to before";
+
+    const idea6 = document.createElement('option');
+    idea6.text = 'Go to a local comedy show';
+
+    //this is the div container where the select elements are held
+    const container = document.getElementById('dates');
+
+    //Remove current ideas in ideas dropdown
+    clear(selectEl)
+
+    //Add new ideas to dropdown
+    selectEl.add(def);
+    selectEl.add(idea1);
+    selectEl.add(idea2);
+    selectEl.add(idea3);
+    selectEl.add(idea4);
+    selectEl.add(idea5);
+    selectEl.add(idea6);
+}
