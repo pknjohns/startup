b612863a1873952caa7be849adb452b6c5fabb7f
Parker Johns <pknjohns@byu.edu>
2024-03-22 18:18:08 -0600

    added requirements for databse.js and made database collections to hold user and history info

--- database.js
+++ database.js
@@ -1,2 +1,10 @@
+const { MongoClient } = require('mongodb');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
 const config = require('./dbConfig.json');
+
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+const db = client.db('startup');
+const userCollection = db.collection('user');
+const historiesCollection = db.collection('score');
