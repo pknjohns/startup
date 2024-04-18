b1d0b65beb14c931ff398a104923d4e8143a8aa8
Parker Johns <pknjohns@byu.edu>
2024-04-17 23:51:46 -0600

    got rid of some unimplemented code in generator

--- src/generator/generator.jsx
+++ src/generator/generator.jsx
@@ -8,8 +8,6 @@ export function Generator(props) {
   return (
     <main className='container-fluid bg-secondary text-center'>
       <div className='body bg-dark text-light'>This is where I would've put my date idea generator if I had had enough time to implement it.</div>
-      <Users userName={props.userName} />
-      <dropDown userName={props.userName} />
     </main>
   );
 }
