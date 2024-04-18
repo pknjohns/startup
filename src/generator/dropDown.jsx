50e855f65eeea9a6ba60df97b97a2c35d5de2c7e
Parker Johns <pknjohns@byu.edu>
2024-04-17 23:47:39 -0600

    made some edits to the generator files

---
+++ src/generator/dropDown.jsx
@@ -0,0 +1,9 @@
+import react from 'react';
+
+export function dropDown(props) {
+    const userName = props.userName
+
+    async function saveHist() {
+        const date = new Date().toLocaleDateString();
+    }
+}
--- src/generator/generator.jsx
+++ src/generator/generator.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './generator.css';
 
-export function Generator() {
-    return (
-      <main className='container-fluid bg-secondary text-center'>
-        <div>generator displayed here</div>
-      </main>
-    );
+import {Users} from './users';
+import {dropDown} from './dropDown';
+
+export function Generator(props) {
+  return (
+    <main className='container-fluid bg-secondary text-center'>
+      <div className='body bg-dark text-light'>This is where I would've put my date idea generator if I had had enough time to implement it.</div>
+      <Users userName={props.userName} />
+      <dropDown userName={props.userName} />
+    </main>
+  );
 }
