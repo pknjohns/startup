f559c8e86ee3f7250fc2225a64a9412833ec9b1a
Parker Johns <pknjohns@byu.edu>
2024-04-17 19:51:53 -0600

    made all pages import their respect css docs

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './about.css';
 
 export function About() {
   const [quote, setQuote] = React.useState('Loading...');
--- src/generator/generator.jsx
+++ src/generator/generator.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './generator.css';
 
 export function Generator() {
     return (
